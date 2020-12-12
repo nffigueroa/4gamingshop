5c0626a3837fb745ebfd77468481d47156809692
Fabian Figueroa <nffigueroag@gmail.com>
2020-12-12 10:51:44 -0500

    dark mode with material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
+import { Provider } from "react-redux";
+import "../styles/globals.css";
+import withReduxStore from "../state/with-redux-store";
+import {
+  createMuiTheme,
+  ThemeProvider,
+  useMediaQuery,
+} from "@material-ui/core";
+import { useMemo } from "react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
+function MyApp({ Component, pageProps, reduxStore }) {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Provider store={reduxStore}>
+        <Component {...pageProps} />
+      </Provider>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default withReduxStore(MyApp);
