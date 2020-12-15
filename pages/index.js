3d4d46b7b9a09b289a49152907b411dd34b323aa
Fabian Figueroa <nffigueroag@gmail.com>
2020-12-15 16:58:27 -0500

    getStaticProps from index page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,14 @@
 import HomePage from "./home";
-export default function Home() {
-  return <HomePage />;
+export default function Home(props) {
+  return <HomePage {...props} />;
+}
+
+export async function getServerSideProps({ query }) {
+  const data = await fetch(process.env.CATEGORYLIST)
+    .then((response) => response.json())
+    .then(({ response: { data } }) => data);
+  console.log(data);
+  return {
+    props: { categoriesDB: data },
+  };
 }
